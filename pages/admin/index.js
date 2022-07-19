7fc441337f329e36cf1f8ba68beec49af7c45e2c
coder-anmol <dev.anmolsharma@gmail.com>
2022-07-19 10:26:28 +0530

    sweet alert added and tested

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,7 @@
 import ModalDefault from "@/components/Modal";
 import TableDemo from "@/components/TableDemo";
 import { Box, Button, useDisclosure, Text } from "@chakra-ui/react";
+import swal from "@sweetalert/with-react";
 
 const Index = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
@@ -42,6 +43,19 @@ const Index = () => {
 
             <Box my={4}>
                 <Button onClick={onOpen}>Open Modal</Button>
+                <Button
+                    onClick={() => {
+                        swal({
+                            text: "How was your experience getting help with this issue?",
+                            icon: "success",
+                            buttons: {
+                                cancel: "Close",
+                            },
+                        });
+                    }}
+                >
+                    Sweet Alert
+                </Button>
             </Box>
         </Box>
     );
